7804fb022132ba5fb8cf4f398c0296842c58854d
mao <1296193245@qq.com>
2022-09-09 12:41:58 +0800

    java_concurrent_programming_thread_pool

--- src/main/java/mao/t10/Test.java
+++ src/main/java/mao/t10/Test.java
@@ -1,21 +1,20 @@
-package mao.t9;
+package mao.t10;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.*;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 /**
  * Project name(项目名称)：java并发编程_线程池
- * Package(包名): mao.t9
+ * Package(包名): mao.t10
  * Class(类名): Test
  * Author(作者）: mao
  * Author QQ：1296193245
  * GitHub：https://github.com/maomao124/
  * Date(创建日期)： 2022/9/9
- * Time(创建时间)： 11:51
+ * Time(创建时间)： 12:27
  * Version(版本): 1.0
  * Description(描述)： 无
  */
@@ -27,20 +26,17 @@ public class Test
      */
     private static final Logger log = LoggerFactory.getLogger(Test.class);
 
-    public static void main(String[] args) throws ExecutionException, InterruptedException, TimeoutException
+    public static void main(String[] args) throws InterruptedException
     {
         ExecutorService threadPool = Executors.newFixedThreadPool(4);
-
-        List<Callable<Integer>> tasks = new ArrayList<>();
 
         for (int i = 0; i < 20; i++)
         {
             int finalI = i;
-
-            tasks.add(new Callable<Integer>()
+            threadPool.submit(new Runnable()
             {
                 @Override
-                public Integer call() throws Exception
+                public void run()
                 {
                     log.debug(finalI + "开始运行");
                     try
@@ -51,14 +47,16 @@ public class Test
                     {
                         e.printStackTrace();
                     }
-                    return finalI;
+                    log.debug(finalI + "结束运行");
                 }
             });
         }
 
-        //执行给定任务，返回已成功完成的任务的结果（即，不抛出异常），如果有的话。
-        //正常或异常返回时，取消未完成的任务。如果在此操作进行时修改了给定的集合，则此方法的结果是不确定的
-        Integer integer = threadPool.invokeAny(tasks,500, TimeUnit.MILLISECONDS);
-        log.debug("结果：" + integer);
+        Thread.sleep(2500);
+        log.debug("开始关闭线程池");
+        //启动有序关闭，其中执行先前提交的任务，但不会接受新任务。如果已经关闭，调用没有额外的效果。
+        //此方法不等待先前提交的任务完成执行。使用awaitTermination来做到这一点
+        threadPool.shutdown();
+
     }
 }
